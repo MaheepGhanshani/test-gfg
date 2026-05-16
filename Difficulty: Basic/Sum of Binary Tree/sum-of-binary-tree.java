9fdec4d57df6edfe8859142c436bdfe2592d85e8
Maheep <121884484+MaheepGhanshani@users.noreply.github.com>
2026-05-16 12:40:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.java
@@ -0,0 +1,27 @@
+/*
+Definition for Node
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    static int sumBT(Node root) {
+        if(root==null){
+            return 0;
+        }
+        int leftsum=sumBT(root.left);
+        int rightsum=sumBT(root.right);
+        return leftsum+rightsum+root.data;
+        // code here
+        
+    }
+}
