29f5b38fc3097a788b360180d613bdf05552ff19
Maheep <121884484+MaheepGhanshani@users.noreply.github.com>
2026-07-04 10:46:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Single Among Doubles in a Sorted/single-among-doubles-in-a-sorted.java
@@ -0,0 +1,23 @@
+class Solution {
+    int single(int[] arr) {
+        // code here
+        int n=arr.length;
+        if(n==1) return arr[0];
+        if(arr[0]!=arr[1]) return arr[0];
+        if(arr[n-1]!=arr[n-2]) return arr[n-1];
+        int low =0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]!=arr[mid-1]&&arr[mid]!=arr[mid+1]) return arr[mid];
+            int f=mid,s=mid;
+            if(arr[mid-1]==arr[mid]) f=mid-1;
+            else s=mid+1;
+            int leftcount=f-low;
+            int rightcount=high-s;
+            if(leftcount%2==0) low=s+1;
+            else high=f-1;
+        }
+        return 747;
+    }
+}
